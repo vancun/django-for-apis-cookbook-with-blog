d06d8afcdcbf3ea5ac0666abe445638fdf465672
ivangeorgiev <ivan.georgiev@gmail.com>
2024-08-12 17:17:47 +0000

    more on the parametrization ids topic

---
+++ src/tests/sandbox/test_parameter_with_fixture_name.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+@pytest.fixture(name="a")
+def given_a():
+    raise NotImplementedError()
+
+
+def b():
+    raise NotImplementedError()
+
+
+@pytest.fixture(name="f")
+def given_f():
+    return lambda a, b: a + b
+
+
+params_test_addition = ((1, 1, 2),)
+
+
+@pytest.mark.parametrize(
+    "a, b, expected",
+    params_test_addition,
+    ids=[f"{a} + {b} == {c}" for a, b, c in params_test_addition],
+)
+def test_addition(a, b, expected, f):
+    assert a + b == expected
